676c8fe8813055d5d756af2a84fef6bf36603a5b
syshotdev <syshotdev@gmail.com>
2024-12-27 15:49:11 -0800

    Added C lsp + library paths + removed from TODO

--- modules/home/development/neovim/config/plugins/lspconfig.lua
+++ modules/home/development/neovim/config/plugins/lspconfig.lua
@@ -8,15 +8,12 @@ local servers = { 'gdscript', 'dockerls', 'lua_ls', 'vimls', 'csharp_ls', 'gradl
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup {
     capabilities = capabilities,
-    -- Add client if you need it
-    on_attach = function(bufnr)
-      print('LSP attached to buffer ' .. bufnr)
-    end
   }
 end
 
+local lspconfig = require('lspconfig')
 -- Specific lua-for-neovim setup
-require 'lspconfig'.lua_ls.setup {
+lspconfig.lua_ls.setup {
   on_init = function(client)
     local path = client.workspace_folders[1].name
     if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
@@ -38,4 +35,35 @@ require 'lspconfig'.lua_ls.setup {
   settings = {
     Lua = {}
   }
+}
+
+-- Requires pkg-config to be installed
+local function get_library_paths(package_name)
+    local pkg_flags = {}
+    local handle = io.popen("pkg-config --cflags --libs " .. package_name)
+    if handle then
+        for flag in handle:read("*a"):gmatch("%S+") do
+            table.insert(pkg_flags, flag)
+        end
+        handle:close()
+    end
+    return pkg_flags
+end
+
+local glib_flags = get_library_paths("glib-2.0")
+
+lspconfig.ccls.setup {
+    cmd = { "ccls" },
+    root_dir = lspconfig.util.root_pattern(".ccls", "compile_commands.json", ".git"),
+    init_options = {
+        cache = {
+            directory = ".ccls-cache";
+        },
+        clang = {
+            extraArgs = vim.tbl_flatten({
+                { "-std=c11", "-x", "c", "--gcc" },
+                glib_flags,
+            }),
+        },
+    },
 }
