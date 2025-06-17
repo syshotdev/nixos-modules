46c3fd3e15e7724ccc6a07f6885f73f46c8bfd5f
syshotdev <syshotdev@gmail.com>
2025-06-16 17:17:31 -0700

    Removed CPUfreq

--- modules/system/optimizations/cpu/default.nix
+++ modules/system/optimizations/cpu/default.nix
@@ -1,8 +1,7 @@
 {...}:
 {
-  services.auto-cpufreq.enable = true; # Optimizations using cpufreq utility
-
   # These settings are probably for laptops
   # But who cares they don't hurt (I think)
   services.thermald.enable = true;
+  powerManagement.cpuFreqGovernor = "performance";
 }
