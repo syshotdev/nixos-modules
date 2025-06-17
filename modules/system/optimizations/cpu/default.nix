4576155f5c73d89690390f2b2804e58a95ecf7a5
syshotdev <syshotdev@gmail.com>
2025-06-16 17:16:04 -0700

    Removing options which don't compile

--- modules/system/optimizations/cpu/default.nix
+++ modules/system/optimizations/cpu/default.nix
@@ -5,5 +5,4 @@
   # These settings are probably for laptops
   # But who cares they don't hurt (I think)
   services.thermald.enable = true;
-  powerManagement.cpuFreqGovernor = "performance";
 }
--- modules/system/optimizations/gpu/default.nix
+++ modules/system/optimizations/gpu/default.nix
@@ -2,8 +2,6 @@
 {
   hardware.opengl = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
     # Mesa is general opengl drivers (I think)
     extraPackages = [ pkgs.mesa.drivers ];
   };
