1965f28b46f49f685d627b5794165127fb5328f5
syshotdev <syshotdev@gmail.com>
2025-06-20 19:31:12 -0700

    Hopefully made modules import args successfully

--- flake.nix
+++ flake.nix
@@ -34,13 +34,14 @@
         })
       ];
     };
+    lib = pkgs.lib;
     specialArgs = {inherit inputs outputs nixpkgs pkgs home-manager;};
   in {
 
     # Everything inside these brackets are attributes, accessable via outputs.attribute
-    systemModules = import ./modules/system { inherit specialArgs;}; # Modules for system
-    homeModules = import ./modules/home { inherit specialArgs;}; # Modules for users
-    scriptModules = import ./modules/scripts { inherit specialArgs;}; # Scripts that I've made
+    systemModules = lib.mapAttrs (_: m: m specialArgs) (import ./modules/system); # Modules for system
+    homeModules = lib.mapAttrs (_: m: m specialArgs) (import ./modules/home); # Modules for users
+    scriptModules = lib.mapAttrs (_: m: m specialArgs) (import ./modules/scripts); # Scripts that I've made
 
     # Custom packages (to be built) not in the nix repository
     # This variable *only* lists the paths to the packages, you have to build them and include them into pkgs.
