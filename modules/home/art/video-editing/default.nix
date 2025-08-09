2b737484c0057bbf25dde561b220f69dbdf83823
syshotdev <syshotdev@gmail.com>
2025-08-08 17:08:24 -0700

    KDenLive still crashes, updated packages

--- modules/home/art/video-editing/default.nix
+++ modules/home/art/video-editing/default.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}:
 {
   home.packages = with pkgs; [
-    ffmpeg # Converter for video to audio and things, is a dependency for a lot of things
+    ffmpeg-full # Converter for video to audio and things, is a dependency for a lot of things
 
     losslesscut-bin # Cut videos easier and faster
-    kdePackages.kdenlive # Video Editor
+    unstable.kdePackages.kdenlive # Video Editor
     glaxnimate # Dependency for KDenLive
 
     handbrake # Turn video formats into other video formats (But with GUI)
--- modules/home/art/video-recording/default.nix
+++ modules/home/art/video-recording/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 {
   home.packages = with pkgs; [
-    ffmpeg # Converter for video to audio and things, is a dependency for a lot of things
+    ffmpeg-full # Converter for video to audio and things, is a dependency for a lot of things
 
     obs-studio # Most used video recorder
     audacity # Audio editing, recording, very useful in some situations
